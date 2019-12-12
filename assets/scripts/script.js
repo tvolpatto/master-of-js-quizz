ab6b3ecfc12d87c7d238a9a884beb6921e569573
Thyago Volpatto <tvolpatto@gmail.com>
2019-12-11 19:48:20 -0700

    showing the answer message and end div, add score

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,65 +1,95 @@
 var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    }
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
 ];
+
 var divQuestion = $("#questions");
+var answerMsg = $("#answer-msg");
 var index = 0;
+var score = 0;
 var numberOfQuestions = questions.length;
-var timer = 15 * numberOfQuestions;
+var countdown = 15 * numberOfQuestions;
+var myTimer;
+
 
 function displayQuestion(index) {
 
-    var question = questions[index];
-    var divTitle = $("<div>");
-    divTitle.text(question.title);
-    divQuestion.append(divTitle);
-    question.choices.forEach(choice => {
-        var divChoice = $("<div>");
-        divChoice.addClass(".choice");
-        divChoice.attr("data-index", index);
-        divChoice.text(choice);
-        divChoice.click(choiceClick);
-        divQuestion.append(divChoice);
-    });
+  var question = questions[index];
+  var divTitle = $("<div>");
+  divTitle.text(question.title);
+  divQuestion.append(divTitle);
+  question.choices.forEach(choice => {
+    var divChoice = $("<div>");
+    divChoice.addClass(".choice");
+    divChoice.attr("data-index", index);
+    divChoice.text(choice);
+    divChoice.click(choiceClick);
+    divQuestion.append(divChoice);
+  
+  });
 
 }
 
 function startQuiz() {
-
-    $("#timer").text(timer);
-    displayQuestion(index);
-    $("#start").addClass("d-none");
-    setInterval(function () {
-        if (timer >= 0) {
-            $("#timer").text(timer--);
-        } else {
-            clearInterval();
-        }
-    }, 1000);
+  
+  $("#timer").text(countdown);
+  displayQuestion(index);
+  $("#start").addClass("d-none");
+  myTimer = setInterval(function () {
+    if (countdown > 0) {
+      $("#timer").text(countdown--);
+    } else {
+      finishQuizz();
+    } 
+  }, 1000);
 }
 
 
 function choiceClick() {
-    var idx = $(this).attr("data-index");
-    var userAnswer = $(this).text();
-    if (questions[idx].answer === userAnswer) {
-        alert("correct");
-    } else {
-        alert("wrong");
-    }
-    index = parseInt(idx) + 1;
+  var idx = parseInt($(this).attr("data-index"));
+  var userAnswer = $(this).text();
+  
+  if (questions[idx].answer === userAnswer) {
+    displayAnswerMsg("text-success", "Correct!");
+    score++;
+  } else {
+    countdown -= 9;
+    displayAnswerMsg("text-danger", "Wrong!");
+  }
+  
+  index = idx + 1;
+  if (index < numberOfQuestions ) {
+    setTimeout(() => {
+      clearScreen();
+      displayQuestion(index);
+    }, 500);
+  } else {
+    finishQuizz();
+  }
+}
 
-    divQuestion.empty();
-    if (index < numberOfQuestions) {
-        displayQuestion(index);
-    }
+function displayAnswerMsg(clazz, text) {
+  answerMsg.removeClass("d-none text-success text-danger").addClass(clazz);
+  answerMsg.text(text);
+}
+
+function clearScreen() {
+  divQuestion.empty();
+  answerMsg.addClass("d-none");
+}
+
+function finishQuizz(){
+  clearInterval(myTimer);
+  clearScreen();
+  divQuestion.addClass("d-none");
+  $("#end").removeClass("d-none");
 }
 
