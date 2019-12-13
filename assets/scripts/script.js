6d971cef740d3308f3641b8630fd10570e223623
Thyago Volpatto <tvolpatto@gmail.com>
2019-12-12 18:57:44 -0700

    adding timer animation; first  parte of the highscores

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -18,7 +18,9 @@ var score = 0;
 var numberOfQuestions = questions.length;
 var countdown = 15 * numberOfQuestions;
 var myTimer;
-
+var playerResults = { initials : "PLR", score: 0 , position: 5};
+var highscores = localStorage.getItem("highscores") != null
+          ? JSON.parse(localStorage.getItem("highscores")) : [];
 
 function displayQuestion(index) {
 
@@ -44,7 +46,10 @@ function startQuiz() {
   displayQuestion(index);
   $("#start").addClass("d-none");
   myTimer = setInterval(function () {
-    if (countdown > 0) {
+    if (countdown >= 0) {
+      if(countdown < 10 ) {
+          animate();
+      }
       $("#timer").text(countdown--);
     } else {
       finishQuizz();
@@ -52,6 +57,11 @@ function startQuiz() {
   }, 1000);
 }
 
+function animate() {
+  $("#timer").addClass("text-danger");
+  $("#timer").animate({ "font-size": "2.5rem" });
+  $("#timer").animate({ "font-size": "2rem" });
+}
 
 function choiceClick() {
   var idx = parseInt($(this).attr("data-index"));
@@ -84,12 +94,67 @@ function displayAnswerMsg(clazz, text) {
 function clearScreen() {
   divQuestion.empty();
   answerMsg.addClass("d-none");
+  $(".form-inline").addClass("d-none");
+  $("#end").addClass("d-none");
+
 }
 
 function finishQuizz(){
   clearInterval(myTimer);
-  clearScreen();
   divQuestion.addClass("d-none");
+  clearScreen();
+  checkHighScores();
+  $("#score").text(score);
   $("#end").removeClass("d-none");
 }
 
+function addToHighScore() {
+  var idx = playerResult.position;
+  if( idx < 5 ) {
+    var aux = {position : 5};
+    if(idx < highscores.length){
+       aux = highscores[idx];
+       aux.position++;
+    } 
+    highscores[idx] = playerResult;
+    playerResult = aux;
+    addToHighScore();
+  }
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+  playerResult = {position : 5};
+  displayHighscores();
+}
+
+function checkHighScores() {
+  var position = highscores.length;
+  if (position < 5) { 
+    playerResults.position = position;
+    return;
+  } else {
+    for (var i = position - 1; i >= 0; i--) {
+      if(playerResult.score > highscores[i].score) {
+        playerResult.position = i; 
+      } else {
+        break;
+      }
+    }
+  }
+  if (playerResults.position < 5) {
+    $(".form-inline").removeClass("d-none");
+  }
+
+}
+
+function displayHighscores() {
+  clearScreen();
+  $("#highscores").removeClass("d-none");
+  var ul = $("list-group");
+  highscores.forEach(hs => {
+    var li = $(`<li class="list-group-item d-flex justify-content-between align-items-center">
+                  ${hs.initials}
+                  <span >${hs.score}</span>
+                </li>`);
+    ul.append(li);
+  });
+
+}
