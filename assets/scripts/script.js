1eb3979d0de4ad3256c0183cba3ea8a7626fd67c
Thyago Volpatto <tvolpatto@gmail.com>
2019-12-12 20:31:15 -0700

    show highscores

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,4 @@
+
 var questions = [
   {
     title: "Commonly used data types DO NOT include:",
@@ -18,9 +19,10 @@ var score = 0;
 var numberOfQuestions = questions.length;
 var countdown = 15 * numberOfQuestions;
 var myTimer;
-var playerResults = { initials : "PLR", score: 0 , position: 5};
+var playerResults = { initials: "PLR", score: 0, position: 5 };
 var highscores = localStorage.getItem("highscores") != null
-          ? JSON.parse(localStorage.getItem("highscores")) : [];
+  ? JSON.parse(localStorage.getItem("highscores")) : [];
+
 
 function displayQuestion(index) {
 
@@ -35,25 +37,25 @@ function displayQuestion(index) {
     divChoice.text(choice);
     divChoice.click(choiceClick);
     divQuestion.append(divChoice);
-  
+
   });
 
 }
 
 function startQuiz() {
-  
+
   $("#timer").text(countdown);
   displayQuestion(index);
   $("#start").addClass("d-none");
   myTimer = setInterval(function () {
     if (countdown >= 0) {
-      if(countdown < 10 ) {
-          animate();
+      if (countdown < 10) {
+        animate();
       }
       $("#timer").text(countdown--);
     } else {
       finishQuizz();
-    } 
+    }
   }, 1000);
 }
 
@@ -66,7 +68,7 @@ function animate() {
 function choiceClick() {
   var idx = parseInt($(this).attr("data-index"));
   var userAnswer = $(this).text();
-  
+
   if (questions[idx].answer === userAnswer) {
     displayAnswerMsg("text-success", "Correct!");
     score++;
@@ -74,9 +76,9 @@ function choiceClick() {
     countdown -= 9;
     displayAnswerMsg("text-danger", "Wrong!");
   }
-  
+
   index = idx + 1;
-  if (index < numberOfQuestions ) {
+  if (index < numberOfQuestions) {
     setTimeout(() => {
       clearScreen();
       displayQuestion(index);
@@ -93,13 +95,14 @@ function displayAnswerMsg(clazz, text) {
 
 function clearScreen() {
   divQuestion.empty();
+  $("#start").addClass("d-none");
   answerMsg.addClass("d-none");
   $(".form-inline").addClass("d-none");
   $("#end").addClass("d-none");
 
 }
 
-function finishQuizz(){
+function finishQuizz() {
   clearInterval(myTimer);
   divQuestion.addClass("d-none");
   clearScreen();
@@ -109,31 +112,33 @@ function finishQuizz(){
 }
 
 function addToHighScore() {
-  var idx = playerResult.position;
-  if( idx < 5 ) {
-    var aux = {position : 5};
-    if(idx < highscores.length){
-       aux = highscores[idx];
-       aux.position++;
-    } 
-    highscores[idx] = playerResult;
-    playerResult = aux;
+  playerResults.initials = $("#initials").val() != "" ? $("#initials").val() : "PLR";
+  playerResults.score = score;
+  var idx = playerResults.position;
+  if (idx < 5) {
+    var aux = { position: 5 };
+    if (idx < highscores.length) {
+      aux = highscores[idx];
+      aux.position++;
+    }
+    highscores[idx] = playerResults;
+    playerResults = aux;
     addToHighScore();
   }
   localStorage.setItem("highscores", JSON.stringify(highscores));
-  playerResult = {position : 5};
+  playerResults = { initials: "PLR", score: 0, position: 5 };
   displayHighscores();
 }
 
 function checkHighScores() {
   var position = highscores.length;
-  if (position < 5) { 
+  if (position < 5) {
     playerResults.position = position;
-    return;
+
   } else {
     for (var i = position - 1; i >= 0; i--) {
-      if(playerResult.score > highscores[i].score) {
-        playerResult.position = i; 
+      if (playerResults.score > highscores[i].score) {
+        playerResults.position = i;
       } else {
         break;
       }
@@ -148,13 +153,14 @@ function checkHighScores() {
 function displayHighscores() {
   clearScreen();
   $("#highscores").removeClass("d-none");
-  var ul = $("list-group");
+  var ul = $(".list-group");
   highscores.forEach(hs => {
     var li = $(`<li class="list-group-item d-flex justify-content-between align-items-center">
-                  ${hs.initials}
-                  <span >${hs.score}</span>
-                </li>`);
+                ${hs.initials}
+                <span>${hs.score}</span>
+              </li>`);
     ul.append(li);
   });
+
 
 }
